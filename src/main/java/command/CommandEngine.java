0dccc474bc0e54174aae075284e8d2003057e57a
Singh-Nirbhay <ns513314@gmail.com>
2026-06-15 14:14:29 +0530

    Fix: commandEngine execute method array validation fix

--- src/main/java/command/CommandEngine.java
+++ src/main/java/command/CommandEngine.java
@@ -1,20 +1,33 @@
 package command;
 
+import java.nio.charset.StandardCharsets;
+
 public class CommandEngine 
 {
 	public static String execute(String[] command) 
-	{
-	     String mainCommand = command[0].toUpperCase();
+	{   
+		
+		if (command==null || command.length ==0) 
+		{
+			return "-ERR Empty command\r\n";
+		}
+		String mainCommand = command[0].toUpperCase();
 	         
 	     switch(mainCommand)
 	     {
 	        case "PING" :
-	        	return "+PONG\r\n";
+	        	if(command.length != 1) 
+	        		return "-ERR wrong number of arguments for 'PING' command\r\n";
+	        	
+	        	 return "+PONG\r\n";
 	        case "ECHO" :
 	        {    
-	        	String bulkString = command[1].toUpperCase();
-	    	    int bulkStringLen = bulkString.length();	
-	        	return ("$"+bulkStringLen+"\r\n"+bulkString+"\r\n");
+	        	if(command.length != 2) return "-ERR wrong number of arguments for 'echo' command\r\n";
+	        	
+	        	String bulkString = command[1];
+		    	int byteLen = bulkString.getBytes(StandardCharsets.UTF_8).length;	
+		        return ("$"+byteLen+"\r\n"+bulkString+"\r\n");
+	        	
 	        }
 	        default :
 	        	return "-ERR unknown command\r\n";
